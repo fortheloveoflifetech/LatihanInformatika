4bc727d5f10d3cf7003aa933bbe1b80982a1f71c
fortheloveoflifetech <rayshaazahra4@gmail.com>
2025-05-27 10:04:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-st.title("Free Download Flowers Picture 🌺")
-st.write(
-    "HD PNG FLOWERS PICTURE 💐"
-)
+#st.title("Free Download Flowers Picture 🌺")
+#st.write(
+#    "HD PNG FLOWERS PICTURE 💐"
+#)
 #st.image("1642be8b4efc2ec9eb7ef9400601df7e.jpg", width=200)
 
 #st.title("Aplikasi Sederhana")
