3939b475e8b0fa14b5d9c213c683e11b7d0ee0f6
fortheloveoflifetech <rayshaazahra4@gmail.com>
2025-05-27 10:04:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,10 @@ st.title("Free Download Flowers Picture 🌺")
 st.write(
     "HD PNG FLOWERS PICTURE 💐"
 )
-st.image("1642be8b4efc2ec9eb7ef9400601df7e.jpg", width=200)
+#st.image("1642be8b4efc2ec9eb7ef9400601df7e.jpg", width=200)
 
-st.title("Aplikasi Sederhana")
-st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+#st.title("Aplikasi Sederhana")
+#st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
 angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
 
 if (angka % 2) ==0:
